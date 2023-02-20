f296a6f796c5fa4b4cc0b7a29cf5d5ce2f708850
Alefantozzi86 <alefantozzi86@gmail.com>
2023-02-20 00:54:14 +0000

    myCard

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,16 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} , ${variables.country}</h3>
+        
+          <ul class="${variables.socialMediaPosition}">
+             <li><a href="https://twitter.com/${variables.twitter}"><i class="fa-brands fa-twitter"></i></a></li>
+    <li><a href="https://github.com/${variables.github}"><i class="fa-brands fa-github"></i></a></li>
+    <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fa-brands fa-linkedin"></i></a></li>
+    <li><a href="https://instagram.com/${variables.instagram}"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
         </div>
     `;
 }
@@ -54,21 +55,23 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://www.qualitydevs.com/wp-content/uploads/2017/12/desarrollador-1288x724.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/D4D35AQEw4tG5uTC45g/profile-framedphoto-shrink_200_200/0/1658963899924?e=1677438000&v=beta&t=1L-qz4MevR26-3gRg0Jr3X2_g6hSzBSceKtbuS3muc0",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "4geeksAcademy",
+    github: "4geeksAcademy",
+    linkedin: "4geeksAcademy",
+    instagram: "4geeksAcademy",
+    name: "Alessandra",
+    lastname: "Fantozzi",
+    role: "Developer",
+    country: "Madrid",
+    city: "España"
   };
   render(window.variables); //render the card for the first time
 
